c655091fa7479dca00c6a531d9945e1e2752f6c7
Hanna Herasimuk <lightwing@yandex.by>
2017-01-30 23:07:36 +0300

    Add modified guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.lowerBorder = 0;
+        this.upperBorder = 0;
+    }
 
     setRange(min, max) {
-
+        this.lowerBorder = min;
+        this.upperBorder = max;
     }
 
     guess() {
-
+        return Math.ceil((this.lowerBorder+this.upperBorder)/2);
     }
 
     lower() {
-
+        this.upperBorder = this.guess();
     }
 
     greater() {
-
+        this.lowerBorder = this.guess();
     }
 }
 
